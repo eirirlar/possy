92c8e3fbb8a3eee1253482d80da8d15051741c20
Eirik Larsen <eirirlar@gmail.com>
2015-03-22 23:01:45 +0100

    ok

--- src/main/scala/com/kodeworks/possy/Possy.scala
+++ src/main/scala/com/kodeworks/possy/Possy.scala
@@ -3,23 +3,45 @@ package com.kodeworks.possy
 import scala.collection.mutable.ListBuffer
 
 object Possy {
+  def calculatePathMin(gridValues: List[List[Int]], path: List[Int]) = {
+    List()
+  }
+
   def calculatePath(gridValues: List[List[Int]], path: List[Int]): List[(Int, Int)] = {
+    val possiblePaths: List[List[(Int, Int)]] = this.possiblePaths(gridValues, path)
+    val distancesPowed: List[List[List[Int]]] = possiblePaths.sliding(2).map((pp: List[List[(Int, Int)]]) => {
+      val pp0 = pp(0)
+      val pp1 = pp(1)
+      pp0.map((p0: (Int, Int)) => {
+        pp1.map((p1: (Int, Int)) => {
+          val x = p1._1 - p0._1
+          val y = p1._2 - p0._2
+          x * x + y * y
+        })
+      })
+    }).toList
+    println("distancesPowed " + distancesPowed)
+    possiblePaths.map(pp => {
+      pp.headOption.getOrElse((-1, -1))
+    })
+  }
+
+  def possiblePaths(gridValues: List[List[Int]], path: List[Int]): List[List[(Int, Int)]] = {
     val possiblePaths: List[List[(Int, Int)]] = path.map(p => {
       val l: ListBuffer[(Int, Int)] = ListBuffer()
-      var i, j = 0
+      var i = 0
       while (i < gridValues.size) {
         var js = gridValues(i)
+        var j = 0
         while (j < js.size) {
           val value = js(j)
-          if(p == value) l.append((i, j))
+          if (p == value) l.append((i, j))
           j += 1
         }
         i += 1
       }
       l.toList
     })
-    possiblePaths.map(pp => {
-      pp.headOption.getOrElse((-1, -1))
-    })
+    possiblePaths
   }
 }
--- src/test/scala/com/kodeworks/possy/TestPossy.scala
+++ src/test/scala/com/kodeworks/possy/TestPossy.scala
@@ -3,31 +3,44 @@ package com.kodeworks.possy
 import org.junit.Test
 
 class TestPossy {
+  import TestPossy._
   @Test
-  def test {
-    val elevationGrid = List(
-      List(1, 1, 2, 3, 4, 5, 8, 7, 6, 5, 4),
-      List(0, 2, 4, 3, 4, 5, 5, 6, 7, 5, 4),
-      List(1, 1, 2, 2, 3, 4, 6, 7, 8, 7, 6),
-      List(3, 2, 2, 2, 2, 2, 3, 4, 3, 4, 3),
-      List(4, 4, 3, 3, 2, 2, 3, 4, 3, 4, 3),
-      List(5, 5, 5, 6, 6, 5, 4, 6, 8, 8, 3),
-      List(4, 5, 6, 7, 7, 7, 6, 7, 6, 7, 8),
-      List(7, 6, 5, 6, 5, 5, 4, 3, 5, 6, 6),
-      List(5, 5, 6, 7, 7, 7, 4, 3, 2, 3, 4),
-      List(4, 4, 4, 5, 6, 5, 6, 7, 4, 5, 1)
-    )
-    val path = List(
-      (0, 0), (1, 1), (2, 1), (3, 2), (3, 3), (4, 3), (5, 4), (6, 6), (7, 6), (8, 5)
-    )
-
-    val pathElevations = path.map(p => elevationGrid(p._2)(p._1))
+  def test1 {
 
     val calculatedPath = Possy.calculatePath(elevationGrid, pathElevations)
 
     println("path: " + path)
     println("pathElevations " + pathElevations)
     println("calculatedPath " + calculatedPath)
-
   }
+
+  @Test
+  def test2 {
+    val calculatedPath = Possy.calculatePathMin(elevationGrid, pathElevations)
+
+    println("path: " + path)
+    println("pathElevations " + pathElevations)
+    println("calculatedPath " + calculatedPath)
+  }
+
+}
+
+object TestPossy {
+  val elevationGrid = List(
+    List(1, 0, 2, 3, 4, 5, 8, 7, 6, 5, 4),
+    List(1, 2, 4, 3, 4, 5, 5, 6, 7, 5, 4),
+    List(0, 1, 2, 2, 3, 4, 6, 7, 8, 7, 6),
+    List(3, 2, 2, 2, 2, 2, 3, 4, 3, 4, 3),
+    List(4, 4, 3, 3, 2, 2, 3, 4, 3, 4, 3),
+    List(5, 5, 5, 6, 6, 5, 4, 6, 8, 8, 3),
+    List(4, 5, 6, 7, 7, 7, 6, 7, 6, 7, 8),
+    List(7, 6, 5, 6, 5, 5, 4, 3, 5, 6, 6),
+    List(5, 5, 6, 7, 7, 7, 4, 3, 2, 3, 4),
+    List(4, 4, 4, 5, 6, 5, 6, 7, 4, 5, 1)
+  )
+  val path = List(
+    (0, 0), (1, 1), (2, 1), (3, 2), (3, 3), (4, 3), (5, 4), (6, 6), (7, 6), (8, 5)
+  )
+  val pathElevations = path.map(p => elevationGrid(p._2)(p._1))
+
 }
