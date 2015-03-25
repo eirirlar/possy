36939145c0a60695d87babf17265a0e0632c9ef9
Eirik Larsen <eirirlar@gmail.com>
2015-03-25 22:38:43 +0100

    ok

---
+++ src/main/scala/com/kodeworks/possy/Dijkstra.scala
@@ -0,0 +1,18 @@
+package com.kodeworks.possy
+
+object Dijkstra {
+
+  type Path[Key] = (Double, List[Key])
+
+  def dijkstra[Key](lookup: Map[Key, List[(Double, Key)]], fringe: List[Path[Key]], dest: Key, visited: Set[Key]): Path[Key] = fringe match {
+    case (dist, path) :: fringe_rest => path match {case key :: path_rest =>
+      if (key == dest) (dist, path.reverse)
+      else {
+        val paths = lookup(key).flatMap {case (d, key) => if (!visited.contains(key)) List((dist + d, key :: path)) else Nil}
+        val sorted_fringe = (paths ++ fringe_rest).sortWith {case ((d1, _), (d2, _)) => d1 < d2}
+        dijkstra(lookup, sorted_fringe, dest, visited + key)
+      }
+    }
+    case Nil => (0, List())
+  }
+}
---
+++ src/test/scala/com/kodeworks/possy/TestDijkstra.scala
@@ -0,0 +1,52 @@
+package com.kodeworks.possy
+
+import Dijkstra._
+import org.junit.Test
+
+class TestDijkstra {
+  @Test
+  def testDijkstra {
+    val lookup: Map[(Int, Int), List[(Double, (Int, Int))]] = Map(
+      (-1,-1) -> List((0.0, (2,0)), (0.0, (2,2))),
+      (0,0)-> List((0.0, (-2,-2))),
+      (0,1)-> List((0.0, (-2,-2))),
+      (1,2)-> List((0.0, (-2,-2))),
+      (2, 0) -> List((1.0, (2, 1))),
+      (2, 1) -> List((5.0, (0, 0)), (4.0, (1, 0)), (2.0, (1, 2))),
+      (2, 2) -> List((1.0, (2,1))),
+      (-2,-2) -> Nil
+    )
+    val start = System.currentTimeMillis()
+    val res = dijkstra[(Int, Int)](lookup, List((0d, List((-1,-1)))), (-2,-2), Set())
+    val time = System.currentTimeMillis() - start
+    println("dijkstra time: " + time)
+    println(res)
+
+    val lookup2: Map[Int, List[(Double, Int)]] = Map(
+      -1 -> List((0.0, 2), (0.0, 8)),
+      0 -> List((0.0, -2)),
+      1-> List((0.0, -2)),
+      7-> List((0.0, -2)),
+      2 -> List((1.0, 5)),
+      5 -> List((5.0, 0), (4.0, 3), (2.0, 7)),
+      8 -> List((1.0, 5)),
+      -2 -> Nil
+    )
+    val start2 = System.currentTimeMillis()
+    val res2 = dijkstra[Int](lookup2, List((0d, List(-1))), -2, Set())
+    val time2 = System.currentTimeMillis() - start2
+    println("dijkstra time: " + time2)
+    println(res2)
+  }
+}
+
+object TestDijkstra {
+  val grid = List(
+    List(1, 0, 2),
+    List(1, 0, 9),
+    List(0, 1, 2)
+  )
+  val path = List(
+    (2, 0), (2, 1), (1, 2)
+  )
+}
