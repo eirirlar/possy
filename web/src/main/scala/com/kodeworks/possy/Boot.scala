c665bcbf98c6ca9e7359ac75b308d62abe1f4cc9
Eirik <eirirlar@gmail.com>
2015-04-07 10:52:57 +0200

    client

--- web/src/main/scala/com/kodeworks/possy/Boot.scala
+++ web/src/main/scala/com/kodeworks/possy/Boot.scala
@@ -1,8 +1,20 @@
 package com.kodeworks.possy
 
 import akka.actor.{Props, ActorSystem}
+import akka.http.Http
+import akka.stream.ActorFlowMaterializer
+import akka.stream.scaladsl.{Sink, Source}
+
+import scala.concurrent.Future
 
 object Boot extends App {
-  val actorSystem = ActorSystem()
-  val httpServer = actorSystem.actorOf(Props(new HttpServer))
+  implicit val system = ActorSystem()
+  implicit val materializer = ActorFlowMaterializer()
+  val serverSource: Source[Http.IncomingConnection, Future[Http.ServerBinding]] =
+    Http(system).bind(interface = "localhost", port = 8080)
+  val bindingFuture: Future[Http.ServerBinding] = serverSource.to(Sink.foreach { connection =>
+    // foreach materializes the source
+    println("Accepted new connection from " + connection.remoteAddress)
+    // ... and then actually handle the connection
+  }).run()
 }
