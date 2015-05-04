cf6525da55ea2ebab6428732a6bd9a726035b2e5
Eirik <eirirlar@gmail.com>
2015-05-04 15:43:49 +0200

    build failure

--- web/src/test/scala/com/kodeworks/possy/TestMatrixPossy.scala
+++ web/src/test/scala/com/kodeworks/possy/TestMatrixPossy.scala
@@ -16,13 +16,26 @@ class TestMatrixPossy {
     val s2 = System.currentTimeMillis()
     val calcPath: List[(Int, Int)] = MatrixPossy.calculatePath(grid, vs)
     println("calcPath : " + calcPath + " " + (System.currentTimeMillis() - s2) + " millis")
+  }
 
+  @Test
+  def testKShortPossy {
+    val s1 = System.currentTimeMillis()
+    val grid = new DenseMatrix(5041, 5041, DemStreamParser.readGrid("C:/dev/src/data/dem/5041.compact"))
+    println("parse: " + (System.currentTimeMillis() - s1) + " millis")
+    val vs = values(grid, path)
+    println("    path : " + path)
+    println("pathVals : " + vs)
+    val s2 = System.currentTimeMillis()
+    val ksp = new KShortPossy(grid)
+    vs.foreach(ksp(_))
+    println("ksp targets " + ksp.targets + " targetLocations " + ksp.costOfPaths.size + " time " + (System.currentTimeMillis() - s2) + " millis")
   }
 }
 
 object TestMatrixPossy {
   val path = List(
-    (1, 1), (2, 1) , (3, 2), (3, 3), (4, 3), (5, 4), (6, 6), (7, 6), (8, 5)
+    (1, 1), (2, 1), (3, 2), (3, 3), (4, 3), (5, 4), (6, 6), (7, 6), (8, 5)
   )
 
   def values(grid: DenseMatrix[Short], path: List[(Int, Int)]): List[Short] =
