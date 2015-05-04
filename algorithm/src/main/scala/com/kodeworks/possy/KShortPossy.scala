e3e0c19dd2de1878ae75048412c4ca42c6e1cbff
Eirik Larsen <eirirlar@gmail.com>
2015-05-04 23:03:37 +0200

    shortest path2 2

--- algorithm/src/main/scala/com/kodeworks/possy/KShortPossy.scala
+++ algorithm/src/main/scala/com/kodeworks/possy/KShortPossy.scala
@@ -17,58 +17,56 @@ class KShortPossy(grid: DenseMatrix[Short]) {
     if (targets.isEmpty) {
       lastDiscoveries = MatrixPossy.discover(grid, target)
     } else {
-        val valueMappedToGridIndices = ListBuffer[Int]()
-        var j = 0
-        while (j < lastDiscoveries.size) {
-          val lastDiscovery: Int = lastDiscoveries(j)
-          val discoveries: IndexedSeq[Int] = discoverNear(grid, target, lastDiscovery, allowedMovement)
-          val distanceList = ListBuffer[(Double, Int)]()
+      val valueMappedToGridIndices = ListBuffer[Int]()
+      var j = 0
+      while (j < lastDiscoveries.size) {
+        val lastDiscovery: Int = lastDiscoveries(j)
+        val discoveries: IndexedSeq[Int] = discoverNear(grid, target, lastDiscovery, allowedMovement)
+        val distanceList = ListBuffer[(Double, Int)]()
 
-          var k = 0
-          while (k < discoveries.size) {
-            val discovery: Int = discoveries(k)
-            val dist = distance(grid, lastDiscovery, discovery)
+        var k = 0
+        while (k < discoveries.size) {
+          val discovery: Int = discoveries(k)
+          val dist = distance(grid, lastDiscovery, discovery)
 
-            val c = combine(1, valueMappedToGridIndices.size + k)
-            distanceList.append((dist.toDouble, c))
-            k += 1
-          }
-          valueMappedToGridIndices ++= discoveries
-          val c = combine(i - 1, j)
-          graph.put(c, distanceList.toList)
-          j += 1
+          val c = combine(1, valueMappedToGridIndices.size + k)
+          distanceList.append((dist.toDouble, c))
+          k += 1
         }
-        lastDiscoveries = valueMappedToGridIndices.result.toIndexedSeq
-        valueMappedToGridIndices.clear
+        valueMappedToGridIndices ++= discoveries
+        val c = combine(1, j)
+        graph.put(c, distanceList.toList)
+        j += 1
       }
-
-//      i = 0
-//      var distanceList = ListBuffer[(Double, Int)]()
-//      while (i < valueMappedToGridIndicesList(0).size) {
-//        val c = combine(0, i)
-//        distanceList.append((0d, c))
-//        i += 1
-//      }
-//      graph.put(start, distanceList.toList)
-//
-//      i = 0
-//      while (i < valueMappedToGridIndicesList(valueMappedToGridIndicesList.size - 1).size) {
-//        val gridIndex: Int = valueMappedToGridIndicesList(valueMappedToGridIndicesList.size - 1)(i)
-//        val c = combine(valueMappedToGridIndicesList.size - 1, i)
-//        graph.put(c, List((0d, end)))
-//        i += 1
-//      }
-
-      graph.put(end, Nil)
-      val fringe = List((0d, List(start)))
-
-      val shortestPath: (Double, List[Int]) = dijkstra(graph.toMap, fringe, end, Set())
-      val shortestPathCoords = shortestPath._2.slice(1, shortestPath._2.size - 1).map(c => {
-        val s: (Int, Int) = split(c)
-        grid.rowColumnFromLinearIndex(valueMappedToGridIndicesList(s._1)(s._2))
-      })
-
+      lastDiscoveries = valueMappedToGridIndices.result.toIndexedSeq
+      valueMappedToGridIndices.clear
     }
+
+    //      i = 0
+    //      var distanceList = ListBuffer[(Double, Int)]()
+    //      while (i < valueMappedToGridIndicesList(0).size) {
+    //        val c = combine(0, i)
+    //        distanceList.append((0d, c))
+    //        i += 1
+    //      }
+    //      graph.put(start, distanceList.toList)
+    //
+    //      i = 0
+    //      while (i < valueMappedToGridIndicesList(valueMappedToGridIndicesList.size - 1).size) {
+    //        val gridIndex: Int = valueMappedToGridIndicesList(valueMappedToGridIndicesList.size - 1)(i)
+    //        val c = combine(valueMappedToGridIndicesList.size - 1, i)
+    //        graph.put(c, List((0d, end)))
+    //        i += 1
+    //      }
+
+    graph.put(end, Nil)
+    val fringe = List((0d, List(start)))
+
+    val shortestPath: (Double, List[Int]) = dijkstra(graph.toMap, fringe, end, Set())
+    //      val shortestPathCoords = shortestPath._2.slice(1, shortestPath._2.size - 1).map(c => {
+    //        val s: (Int, Int) = split(c)
+    //        grid.rowColumnFromLinearIndex(valueMappedToGridIndicesList(s._1)(s._2))
+    //      })
 
     targets += target
     this
