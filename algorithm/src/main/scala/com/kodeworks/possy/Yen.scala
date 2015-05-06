0da05280cf882bbc8dda4714a646894e41dfc565
Eirik <eirirlar@gmail.com>
2015-05-06 09:45:44 +0200

    ok

---
+++ algorithm/src/main/scala/com/kodeworks/possy/Yen.scala
@@ -0,0 +1,8 @@
+package com.kodeworks.possy
+
+
+object Yen {
+  def kShortestPath(graph: AnyVal, from: Int, to: Int, k: Int) = {
+
+  }
+}
---
+++ algorithm/src/test/scala/com/kodeworks/possy/TestFloydWarshal.scala
@@ -0,0 +1,34 @@
+package com.kodeworks.possy
+
+import junit.framework.Test
+import TestFloydWarshal._
+import org.apache.commons.graph.algorithm.path.AllPairsShortestPath
+import org.apache.commons.graph.domain.basic.DirectedGraphImpl
+
+class TestFloydWarshal {
+
+  @Test
+  def testAllPairsShortestPath {
+//    FloydWarshall.allPairsShortestPath(nodes,)
+
+  }
+}
+
+object TestFloydWarshal {
+  val nodes = Set(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+  val grid = List(
+    List(1, 0, 2),
+    List(1, 0, 9),
+    List(0, 1, 2)
+  )
+  val path = List(
+    (2, 0), (2, 1), (1, 2) // 0, 1, 9
+    /*
+    target, coords, nodeids
+    0: 0,1  1,1  2,0 // 2  5  7
+    1: 0,0  1,0  2,1 // 1  4  8
+    9: 1,2           // 6
+     */
+  )
+}
